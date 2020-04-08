29af2c65fc1ed51b9f6caa16f7c90b71adf63d36
paul8765-bit <55503865+paul8765-bit@users.noreply.github.com>
2020-04-08 10:26:03 +0100

    Add Program.Main so it can be run on command

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ImageClueIntegrationTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                ImageClueIntegrationTestClass testClass = new ImageClueIntegrationTestClass();
+                Console.WriteLine("\n----------About to run 2 integration tests-------------\n");
+                Console.WriteLine("\n----------First Test: Basic Flow with 4 people -------------\n");
+                testClass.TestBasicFlowWithFourPeople();
+                Console.WriteLine("\n----------Second Test: Basic Flow with 9 people -------------\n");
+                testClass.TestBasicFlowWithNinePeople();
+                Console.WriteLine("\n----------All tests successfully completed -------------\n");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("\n----------Test failed -------------\n");
+                Console.WriteLine("\n----------" + e.Message + " -------------\n");
+                Console.WriteLine("\n----------" + e.StackTrace + " -------------\n");
+                throw e;
+            }
+        }
+    }
+}
